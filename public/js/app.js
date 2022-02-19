3403e11ffa9d3bbc964768b4974dbc2c0cf225e5
Junhan93 <junhan.3008@gmail.com>
2022-02-20 02:28:18 +0800

    setup app for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -16,7 +16,7 @@ weatherForm.addEventListener('submit', (e) => {
     errorMessage.textContent = '';
     const address = search.value;
 
-    fetch('http://localhost:3000/weather?address=' + address).then((response) => {
+    fetch('/weather?address=' + address).then((response) => {
         response.json().then((data) => {
             if (data.error) {
                 errorMessage.style.color = 'Red';
